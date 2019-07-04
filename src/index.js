4f575356a8553ccb96ddc615845fbac02747b4ce
mabry1985 <mabry1985@gmail.com>
2019-07-04 09:56:59 -0700

    add materialize framework

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+import './css/styles.css';
+import './triangle.js';
+
+$(document).ready(function () {
+
+});
